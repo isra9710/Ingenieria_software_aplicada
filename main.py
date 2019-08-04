c7d84f865880a9907fad9b04983d93ce40d598f2
isra9710 <isra.rios.con@gmail.com>
2019-08-04 12:48:50 -0500

    27.5

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from flask import Flask, render_template, request, session, flash, redirect, url_for
-
-
-from sqlalchemy import or_, and_
-
 import os
 app = Flask(__name__)
 SECRET_KEY = "my_secret_key"
@@ -136,53 +132,6 @@ def eliminarMedicamento(id):
     return mostarMedicamentos()
 
 
-
-@app.route("/mostrarMedicamentos")
-def crudMedicamentos():
-    medicamentos = ProductoInventario.query.all()
-    administradores = Usuario.query.filter_by(tipo="Administrador")
-    proveedores = Proveedor.query.all()
-    return render_template("mostrarMedicamentos.html", medicamentos=medicamentos, administradores=administradores, proveedores=proveedores)
-
-
-@app.route("/agregarMedicamento", methods=['GET', 'POST'])
-def agregarMedicamento():
-    #if "username" in session:
-   if request.method == "POST":
-        nombre = request.form.get("nombre")
-        emple = request.form.get("emple")
-        proveedor = request.form.get("proveedor")
-        cantidad = request.form.get("cantidad")
-        porcion = request.form.get("porcion")
-        descripcion = request.form.get("descripcion")
-        fechaE = request.form.get("fechaE")
-        fechaV = request.form.get("fechaV")
-        precio = request.form.get("precio")
-        f = request.files['file']
-        folder = os.path.realpath(__file__).replace('\\', '/').split('/')[0:-1]
-        f.save('/'.join(folder) + '/static/' + f.filename)
-        nombreAux = None
-        if nombre is not None:
-            medicamentos = ProductoInventario.query.filter_by(nombre=nombre)
-            if medicamentos is not None:
-                for e in medicamentos:
-                    nombreAux = e.nombre
-        else:
-            flash("Selecciona el nombre de un administrador, no se agrego la universidad")
-            return crudMedicamentos()
-        if nombre == nombreAux:
-            flash("Ese medicamento ya existe!")
-            return crudMedicamentos()
-        else:
-            print("Estas procediendo a registrarla")
-            administrador = Usuario.query.filter_by(nombre=emple).first()
-            proveedor = Proveedor.query.filter_by(nombre=proveedor).first()
-            medicamento = ProductoInventario(proveedor.idProveedor, administrador.idUsuario, nombre, cantidad, porcion, descripcion, fechaE, fechaV, precio, f.filename)
-            #flash("Se agrego medicamento con exito")
-            db.session.add(medicamento)
-            db.session.commit()
-            return crudMedicamentos()
-
 @app.route('/mostrarEmpleados')
 def mostrarEmpleados():
     empleados = Usuario.query.filter_by(tipo="Empleado")
@@ -313,16 +262,12 @@ def validarNombreP(nombreO, nombreN):
             return True
 #Aqui terminan los CRUD
 
-###Aqui prueba para git###
-####prueba 3####
-
-
-
-
 
 @app.route("/prueba")
 def prueba():
     return render_template("index.html")
+
+
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
