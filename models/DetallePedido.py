408c7d0cff0f22e3fbdb9c3f0ec7eeafd4b18c1b
isra9710 <isra.rios.con@gmail.com>
2019-08-07 13:05:22 -0500

    28.2

--- models/DetallePedido.py
+++ models/DetallePedido.py
@@ -7,12 +7,11 @@ class DetallePedido(db.Model):
     idDetallePedido = db.Column(db.Integer, primary_key=True)
     cantidad = db.Column(db.Integer)
     idPedido = db.Column('idPedido', db.Integer, db.ForeignKey("Pedido.idPedido"))
-    idProducto_inventario = db.Column('idMedicamento', db.Integer, db.ForeignKey("Medicamento.idMedicamento"))
+    idMedicamento = db.Column('idMedicamento', db.Integer, db.ForeignKey("Medicamento.idMedicamento"))
 
     def __init__(self, idPedido, idMedicamento):
         self.idPedido = idPedido
-        self.idProducto_inventario = idProducto_inventario
-        self.idProducto_inventario = idProducto_inventario
+        self.idMedicamento = idMedicamento
 
     def __repr__(self):
         return ''
